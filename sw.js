b09af42c33983d28d78494263b3f5f4437c1fa96
DESKTOP-RV9OKJA\milton.velillac <milton.velillac@gmail.com>
2019-02-08 21:32:37 -0500

    second commit

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ const INMUTABLE_CACHE = 'inmutable-v1';
 // aqui va lo que hicimos nosotros, por ejemplo la libreria de jquery no iria aqui, porque esa
 // no la hicimos nosotros, mas bien iria en el app shell inmutable
 const APP_SHELL = [
-    '/',
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
